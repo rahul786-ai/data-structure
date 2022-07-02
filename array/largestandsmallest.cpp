b41d6384502354105270ca29286ca99b4c21e95a
rahul shah <rkofficial568@gmail.com>
2022-07-02 21:22:48 +0530

    init

---
+++ array/largestandsmallest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<climits>
+ using namespace std;
+
+ //find the largest and smallest number in array
+
+ int main(){
+    int n,key;
+    cin>>n;
+
+    int a[1000];
+    for(int i = 0; i<n; i++){
+        
+    }
+ }
