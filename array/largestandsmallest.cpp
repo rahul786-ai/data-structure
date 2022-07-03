c46cbde90eacce6e0b9f6cffddf63eebdfc7c72d
rahul shah <rkofficial568@gmail.com>
2022-07-03 13:58:51 +0530

    init

--- array/largestandsmallest.cpp
+++ array/largestandsmallest.cpp
@@ -10,6 +10,19 @@
 
     int a[1000];
     for(int i = 0; i<n; i++){
+      cin >>a[i];
         
     }
+    //algorithm to find largest and smallest number
+    int largest = INT_MIN;
+    int smallest = INT_MAX;
+
+    for(int i = 0; i<n; i++){
+      largest = max(largest,a[i]);
+      smallest = min(smallest,a[i]);
+    
+    }
+    cout<<"largest "<<largest <<endl;
+
+
  }
